8d31c2ae37afca004e30fec7b19d2bd74dd0aa3f
あろえりーな <4443321+aloerina01@users.noreply.github.com>
2020-02-06 18:32:59 +0900

    Modify to design FeedItemMenu

--- src/components/FeedItemMenu.tsx
+++ src/components/FeedItemMenu.tsx
@@ -1,12 +1,37 @@
 import * as React from "react";
 import { FeedItemMenuProps } from "../types/FeedItemMenu";
+import { closeFeedItemMenu } from "../usecases/CloseFeedItemMenu";
 
 export const FeedItemMenu: React.FC<FeedItemMenuProps> = ({
   isDisplay,
   data
 }) => {
   if (!isDisplay || !data) {
-    return null;
+    return (
+      <div className="modal-layer">
+        <div className="slide-up-modal modal-feeditem-menu">a</div>
+      </div>
+    );
   }
-  return <div>a</div>;
+  return (
+    <div className="modal-layer show" onClick={closeFeedItemMenu}>
+      <div className="slide-up-modal modal-feeditem-menu">
+        <div className="modal-feeditem-menu-title">
+          <div>{data.category}</div>
+          <div>フォロー</div>
+        </div>
+        <div className="modal-feeditem-menu-list">
+          <ul>
+            <li>共有</li>
+            <li>この記事を表示しない</li>
+            <li>{data.category}に関心がない</li>
+            <li>{data.publisher}の記事を表示しない</li>
+            <li>興味を管理</li>
+            <li>ご意見・ご要望</li>
+          </ul>
+          <div>閉じる</div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/usecases/CloseFeedItemMenu.ts
+++ src/usecases/CloseFeedItemMenu.ts
@@ -1,7 +1,8 @@
 import { ModalStateStore } from "../stores/ModalStateStore";
 import { ModalType, ModalState } from "../types/ModalState";
 
-export function openFeedItemMenu() {
+export function closeFeedItemMenu(event) {
+  event.preventDefault();
   const newState: ModalState = {
     activeModalType: ModalType.None,
     data: null
