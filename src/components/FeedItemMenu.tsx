a8bc21f1af5e35c4c99c3101424287559f65eddb
あろえりーな <4443321+aloerina01@users.noreply.github.com>
2020-02-07 12:39:57 +0900

    Modify feedItemMenu component design

--- src/components/FeedItemMenu.tsx
+++ src/components/FeedItemMenu.tsx
@@ -16,20 +16,24 @@ export const FeedItemMenu: React.FC<FeedItemMenuProps> = ({
   return (
     <div className="modal-layer show" onClick={closeFeedItemMenu}>
       <div className="slide-up-modal modal-feeditem-menu">
-        <div className="modal-feeditem-menu-title">
-          <div>{data.category}</div>
-          <div>フォロー</div>
+        <div className="modal-feeditem-menu-header">
+          <div className="title">{data.category}</div>
+          <div className="follow-button">フォロー</div>
         </div>
-        <div className="modal-feeditem-menu-list">
-          <ul>
-            <li>共有</li>
-            <li>この記事を表示しない</li>
-            <li>{data.category}に関心がない</li>
-            <li>{data.publisher}の記事を表示しない</li>
-            <li>興味を管理</li>
-            <li>ご意見・ご要望</li>
+        <div className="modal-feeditem-menu-body">
+          <ul className="menu-list">
+            <li className="menu-list-item">共有</li>
+            <li className="menu-list-item">この記事を表示しない</li>
+            <li className="menu-list-item">{data.category}に関心がない</li>
+            <li className="menu-list-item">
+              {data.publisher}の記事を表示しない
+            </li>
+            <li className="menu-list-item">興味を管理</li>
+            <li className="menu-list-item">ご意見・ご要望</li>
           </ul>
-          <div>閉じる</div>
+        </div>
+        <div className="modal-feeditem-menu-footer">
+          <div className="close-button">閉じる</div>
         </div>
       </div>
     </div>
